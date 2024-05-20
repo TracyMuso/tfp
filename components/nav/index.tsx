f07296c28e523f2623bac6ae845abe462937eb5b
TracyMuso <musongoletracy@gmail.com>
2024-05-20 15:54:37 +0200

    feat(nav-menu): create and style desktop nav-menu

---
+++ components/nav/index.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
+import GithubIcon from "../../public/images/icons/github-mark-white.svg";
+import { NavMenuData } from "@/constants/data";
+
+const NavMenu = () => {
+  return (
+    <nav className="flex justify-between w-full items-center px-6 py-3 m-0 text-white bg-[#E6739F]">
+      <div className="logo text-2xl font-bold cursor-pointer">TM</div>
+      <div className="flex justify-around gap-6 items-center">
+        {NavMenuData.map((item, idx) => {
+          return (
+            <Link
+              key={idx}
+              className="hover:underline hover:decoration-1"
+              href={item.url}
+            >
+              {item.title}
+            </Link>
+          );
+        })}
+      </div>
+      <div className="flex items-center justify-between gap-4">
+        <Link href={"https://github.com/TracyMuso"}>
+          <Image
+            alt="git-icon"
+            width={30}
+            height={30}
+            objectFit=""
+            src={GithubIcon}
+          />
+        </Link>
+        <button className="border-none rounded py-2 px-3">Resume</button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavMenu;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,86 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        purple: {
+          100: "#EDCCFF",
+          200: "#A152CC",
+          300: "#793D99",
+          400: "#531773",
+          500: "#410064",
+        },
+        pink: {
+          100: "#FFBBB8",
+          200: "#FF180E",
+        },
+        yellow: {
+          50: "#FFF2CC",
+          100: "#FFE599",
+          200: "#FFD866",
+          300: "#FFCB33",
+          400: "#FFC10D",
+        },
+        gray: {
+          100: "#878787",
+          200: "#4E4E4E",
+          300: "#1E1D1D",
+        },
+        green: {
+          100: "#A4D498",
+          200: "#21A501",
+        },
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
+        Inter: ["Inter", "sans-serif"],
+      },
+      lineHeight: {
+        10: "1rem",
+        11: "2rem",
+        12: "3rem",
+        14: "4rem",
+        16: "5rem",
+        18: "6rem",
+      },
+      fontSize: {
+        xs: "0.7rem",
+        sm: "0.85rem",
+        m: "0.9375rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.6rem",
+        "3xl": "2.1rem",
+        "4xl": "2.7rem",
+        "5xl": "3.3rem",
+      },
+      fontWeight: {
+        thin: "100",
+        hairline: "100",
+        extralight: "200",
+        light: "300",
+        normal: "400",
+        medium: "500",
+        bold: "600",
+        extrabold: "700",
+        "extra-bold": "700",
+      },
+      borderRadius: {
+        none: "0",
+        DEFAULT: "0.25rem",
+        md: "0.5rem",
+        lg: "0.75rem",
+        xl: "1rem",
+        "2xl": "1.25rem",
+        "3xl": "1.5rem",
+        "4xl": "1.75rem",
       },
     },
   },
-  plugins: [],
-}
-export default config
+};
