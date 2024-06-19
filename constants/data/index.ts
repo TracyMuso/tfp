5f1a8062963543947af24ac7f7efba0e48d3bda4
TracyMuso <musongoletracy@gmail.com>
2024-06-19 12:48:02 +0200

    feat(services-section): add service card data

--- constants/data/index.ts
+++ constants/data/index.ts
@@ -1,3 +1,5 @@
+import { type serviceCardProps } from "@/types";
+
 interface NavMenu {
   url: string;
   title: string;
@@ -19,5 +21,44 @@ export const NavMenuData: NavMenu[] = [
   {
     title: "Contact",
     url: "#contact",
+  },
+];
+
+export const ServiceCardData: serviceCardProps[] = [
+  {
+    imgUrl: "/icons/settings-svgrepo-com.svg",
+    alt: "settings icon",
+    title: "Easy to Maintain",
+    text: "Assured long-term maintainability",
+  },
+  {
+    imgUrl: "/icons/frame-alt-svgrepo-com.svg",
+    alt: "frame icon",
+    title: "Pixel Perfect",
+    text: "Engaging and eye-catching designs",
+  },
+  {
+    imgUrl: "/icons/magic-wand-wizard-svgrepo-com.svg",
+    alt: "magic-wand-icon",
+    title: "Clean Design",
+    text: "Very professional and highly functional websites",
+  },
+  {
+    imgUrl: "/icons/alarm-svgrepo-com.svg",
+    alt: "alarm-icon",
+    title: "On time Project",
+    text: "Delivering excellent quality work in good time",
+  },
+  {
+    imgUrl: "/icons/support-services-svgrepo-com.svg",
+    alt: "support-icon",
+    title: "Full Support",
+    text: "Need to tweak your website a bit, hit me up",
+  },
+  {
+    imgUrl: "/icons/thumbs-up-svgrepo-com.svg",
+    alt: "thumbs-up-icon",
+    title: "Built with SASS",
+    text: "Modern Websites built for scalability and efficiency",
   },
 ];
