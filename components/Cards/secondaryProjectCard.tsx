2c5f1567d6f66c1b0fe79a1dcc3784ac7aaa2143
TracyMuso <musongoletracy@gmail.com>
2024-07-01 16:52:57 +0200

    feat(projects-card): make extra project card a reusable component

--- components/Cards/secondaryProjectCard.tsx
+++ components/Cards/secondaryProjectCard.tsx
@@ -1,28 +1,55 @@
 import Image from "next/image";
+import Link from "next/link";
+import { type Projects } from "@/types";
 
-const secondaryProjectCard = () => {
+const SecondaryProjectCard = ({
+  img,
+  name,
+  description,
+  gitUrl,
+  liveUrl,
+  technologies,
+}: Projects) => {
   return (
-    <div>
-      <Image
-        src={"/Images/sth.png"}
-        width={300}
-        height={320}
-        alt="project ui"
-      />
-      <div>
-        <h4>Other project</h4>
-        <p>desription</p>
+    <div className="w-[300px] p-5 m-0 h-[250px] rounded-md border border-black mx-auto">
+      <div className="flex flex-col gap-3">
+        <h4 className="font-bold text-xl">{name}</h4>
+        <p className="leading-6">{description}</p>
         <hr />
-        <div>
-          <span>tag1</span>
-          <span>tag1</span>
-          <span>tag1</span>
+        <div className="block">
+          {technologies?.map((item, idx) => {
+            return (
+              <span key={idx} className="inline-block px-2">
+                {item}
+              </span>
+            );
+          })}
         </div>
-        <div>
-          <button>See live</button>
-          <button>source code</button>
+        <div className="flex justify-between">
+          <Link href={liveUrl} className="block">
+            See live
+            <Image
+              className="inline"
+              src={img}
+              height={20}
+              width={20}
+              alt="github icon"
+            />
+          </Link>
+          <Link href={gitUrl}>
+            Source code
+            <Image
+              className="inline"
+              src={"/icons/github-mark-white.svg"}
+              height={20}
+              width={20}
+              alt="github icon"
+            />
+          </Link>
         </div>
       </div>
     </div>
   );
 };
+
+export default SecondaryProjectCard;
