8606908f8f8b9281f063dbc4e25812d4b6be1296
TracyMuso <musongoletracy@gmail.com>
2024-07-14 22:32:28 +0200

    feat(update-about): update work experience styles on about section

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -41,8 +41,8 @@ const About = () => {
             height={500}
           />
         </div>
-        <article className="flex flex-col text-right w-[60%]">
-          <h2 className="text-4xl font-bold">About me</h2>
+        <article className="flex flex-col justify-between text-right w-[60%]">
+          <h2 className="text-4xl">About me</h2>
           <p className="leading-7 py-4">
             Hello, my name is Tracy Musongole. I`m a software developer based in
             Zambia. My time spent living in Moscow, Russia taught me how life
@@ -57,7 +57,7 @@ const About = () => {
             out.
           </p>
           <div className="pt-4">
-            <h4 className="pt-5 font-bold text-xl">
+            <h4 className="pt-5 text-2xl">
               Here are a couple of technologies I work with
             </h4>
             <div className="ml-auto flex items-center justify-between w-2/3">
@@ -80,35 +80,45 @@ const About = () => {
           </div>
         </article>
       </div>
-      <div className="py-8">
-        <h2 className="text-2xl text-center font-bold py-4">
+      <div className="pb-6 pt-16 w-full px-12">
+        <h2 className="text-3xl font-semibold text-center py-8">
           Where I`ve worked
         </h2>
-        <div className="flex justify-between w-full gap-6">
-          <div className="w-[500px] border border-[#cc0e74] rounded-lg p-6 shadow flex flex-col gap-5 ">
+        <div className="w-[85%] mx-auto gap-6">
+          <div className="w-[500px] mr-auto border-l-[#f78ca2] border-s p-3 flex flex-col gap-4 ">
             <div className="flex justify-between">
-              <h4 className="font-bold">The hot platform</h4>
-              <span className="italic ">May 2023 - present</span>
+              <Link
+                href={"https://thehotplatform.com"}
+                className="font-bold underline text-white"
+              >
+                The hot platform
+              </Link>
+              <span className="italic">May 2023 - present</span>
             </div>
-            <p className="italic">
+            <p className="italic text-[#ff9b54]">
               Building and optimizing core components of the web-app
             </p>
-            <ul className="pl-4 py-2">
+            <ul className="pl-4 py-2 text-sm flex flex-col items-start gap-1">
               <li>Successfully migrated website from React to nextjs</li>
               <li>Worked as frontend lead developer for a month</li>
               <li>Evaluate user feedback and optimize website accordingly</li>
             </ul>
           </div>
-          <div className="w-[500px] border border-[#cc0e74] rounded-lg p-6 shadow ">
+          <div className="w-[500px] ml-auto p-3 flex flex-col gap-4 border-s border-l-[#f78ca2]">
             <div className="flex justify-between">
-              <h4 className="font-bold">Ipahive</h4>
+              <Link
+                href={"https://linkedin.com/in/company/ipahive"}
+                className="font-bold underline text-white"
+              >
+                Ipahive
+              </Link>
               <span className="italic ">May 2023 - present</span>
             </div>
-            <p>
+            <span className="italic text-[#ff9b54]">
               Worked as a solo frontend engineer building the growth capital
               web-app from scratch
-            </p>
-            <ul className="pl-4 py-2">
+            </span>
+            <ul className="pl-4 py-2 text-sm flex flex-col items-start gap-1">
               <li>
                 Built and maintained a robust micro-loan platform for users with
                 even minimal technical know-how.
